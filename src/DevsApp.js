6f6a11902db3faa247bccdc9cfa7e9c45aac171b
BrandonTriana <trianab659@gmail.com>
2022-01-22 22:23:41 -0500

    loginScreen almost done missing google auth

--- src/DevsApp.js
+++ src/DevsApp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { LoginScreen } from './components/auth/LoginScreen';
+import './styles/devsApp.css'
 
 
 export const DevsApp = () => {
--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import '../../styles/loginScreen.css'
 
 export const LoginScreen = () => {
   return(
-      <div>
-          <div className='login-logo-container'>
-            <img src='./img/logo' alt='logo'/>
-            <img src='./img/letterlogo' alt='logo'/>
+      <div className='login-container'>
+          <div className='login-left-container'>
+            <img src='./img/logo.png' alt='logo'/>
+            <img src='./img/letterlogo.png' alt='logo'/>
+          </div>
+          <div className='login-right-container'>
+            <div className='login-text-container'>
+                <h1>Lorem ipsum dolor</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <div className='singing-google'>
+                </div>
+                <p>© 2020 Devs_United - <span>BETA</span></p>
+            </div>
           </div>
       </div>
   )
